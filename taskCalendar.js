4d526e7cf36e6761a76b7c2261e4ab38ef657a66
anbordios <annbordios15@gmail.com>
2024-10-01 23:07:32 +0800

    Add files via upload

---
+++ taskCalendar.js
@@ -0,0 +1,11 @@
+let day = "Monday";
+switch(day){
+  case "Monday":
+    console.log("Finish your assignments.");
+    break;
+  case "Friday":
+    console.log("Wash the laundry.");
+    break;
+  default:
+    console.log("It's either Monday or Friday.");
+}
