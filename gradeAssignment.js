00066df3cb34cd240fc0461986cc943db286648e
anbordios <annbordios15@gmail.com>
2024-10-01 22:50:48 +0800

    Add files via upload

---
+++ gradeAssignment.js
@@ -0,0 +1,10 @@
+let score = 85;
+if(score >= 90){
+  console.log("Excellent");
+}else if(score >= 80-89){
+  console.log("Good");
+}else if(score <= 70-79){
+  console.log("Fair");
+}else{
+  console.log("Needs Improvement");
+}
